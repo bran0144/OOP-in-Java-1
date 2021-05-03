2091999abcdc0aa941adf4c5df290f5e6a64011f
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-05-03 17:01:29 -0500

    more bad code

--- src/Account.java
+++ src/Account.java
@@ -7,6 +7,10 @@ public class Account {
     public void holderVerified() {
         this.isVerified = true;
     }
+
+    public void closeAccount() {
+        this.isClosed = true;
+    }
     public void deposit(BigDecimal amount) {
         if(this.isClosed)
             return;
@@ -14,6 +18,8 @@ public class Account {
     }
     public void withdraw(BigDecimal amount) {
         if (!this.isVerified)
+            return;
+        if (this.isClosed)
             return;
     }
 }
