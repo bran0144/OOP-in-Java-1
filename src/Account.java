0b260568e979f982bbcd5ee8aeec44fbe31cf7a3
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-05-03 17:18:12 -0500

    made even more complicated

--- src/Account.java
+++ src/Account.java
@@ -3,7 +3,15 @@ import java.math.BigDecimal;
 public class Account {
     private boolean isVerified;
     private boolean isClosed;
+    private boolean isFrozen;
     private BigDecimal balance;
+
+    private AccountUnfrozen onUnfrozen;
+
+    public Account(AccountUnfrozen onUnfrozen) {
+        this.balance = BigDecimal.ZERO;
+        this.onUnfrozen = onUnfrozen;
+    }
 
     public void holderVerified() {
         this.isVerified = true;
@@ -12,9 +20,22 @@ public class Account {
     public void closeAccount() {
         this.isClosed = true;
     }
+
+    public void freezeAccount() {
+        if (this.isClosed)
+            return;
+        if (!this.isVerified)
+            return;
+        this.isFrozen = true;
+    }
+
     public void deposit(BigDecimal amount) {
         if(this.isClosed)
             return;
+        if (this.isFrozen) {
+            this.isFrozen = false;
+            this.onUnfrozen.handle();
+        }
         this.balance = this.balance.add(amount);
 
     }
